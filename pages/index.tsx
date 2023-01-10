11d706e391d1dc7bdc44759d73e517a003239553
William de Angelo <wdangelo1983@gmail.com>
2023-01-10 10:29:36 -0300

    feat(home): melhorias na pagina principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Maintence from '../components/maintence'
 import SocialBar from '../components/SocialBar'
 import NavBar from '../components/NavBar'
+import Image from 'next/image'
 
 
 
@@ -22,10 +22,27 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
 
-        <h1>TWS Soluções em Tecnologia</h1>
+        
+        <div className={styles.frameBody}>
 
-        <h2>Site em Construção</h2>
-        <Maintence />
+            <Image className={styles.indexImage} src={'/security.jpg' } width={900} height={700} />
+
+
+          <div className={styles.indexText} >
+            <h1>TWS Soluções em Tecnologia</h1>
+            <p>
+            Nosso propósito e objetivo principal é Humanizar as relações entre Clientes e Processos, conhecendo a fundo o modelo de negócio, adotando conceitos de entrega e feedback contínuo, onde o Cliente estará SEMPRE integrado em todos os passos da entrega de seu projeto.
+            </p>
+            <p>
+            Nossos produtos são voltados para pequenas, médias e grandes empresas, com o intuito de melhorar sua performance e manter maior segurança cibernética na gestão de TI.
+            </p>
+            <p>
+            Projetos elaborados pelos nossos especialistas de TI, em vários parques diferentes como: Infraestrutura, Cybersegurança, Suporte, Desenvolvimento e Banco de Dados usando ferramentas ágeis, para a otimização dos processos dos nossos clientes.
+            </p>
+          </div>
+
+        </div>
+
 
       </main>
 
